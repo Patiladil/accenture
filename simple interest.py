bc00074b10359886742489894433092ef0683b21
Patiladil <112052368+Patiladil@users.noreply.github.com>
2022-08-25 09:53:46 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,5 @@
+principal =int(input("Enter principal amount = "))
+rate = int(input("Enter rate = "))
+time = int(input("Enter time = "))
+si = principal * rate * time / 100
+print("Simple interest = ",si)
